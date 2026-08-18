7471d3209641bba04daf47da8348f86198d399e8
treymouledoux <72358826+treymouledoux@users.noreply.github.com>
2026-08-18 18:58:54 -0500

    genesis commit

---
+++ portfolio-serve/src/main.rs
@@ -0,0 +1,12 @@
+use axum::Router;
+use tower_http::services::ServeDir;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().nest_service("/portfolio-static", ServeDir::new("assets"));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:560").await.unwrap();
+    println!("Serving static files on http://localhost:560");
+    
+    axum::serve(listener, app).await.unwrap();
+}
