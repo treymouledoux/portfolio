a5b900631d9ffc8d0b1725a0eab07de3dd3438d5
treymouledoux <72358826+treymouledoux@users.noreply.github.com>
2026-08-19 00:30:13 -0500

    change to fallback_service for serving static files

--- portfolio-serve/src/main.rs
+++ portfolio-serve/src/main.rs
@@ -3,10 +3,11 @@ use tower_http::services::ServeDir;
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().nest_service("/portfolio-static", ServeDir::new("assets"));
+    let app = Router::new().fallback_service(
+        ServeDir::new("assets").append_index_html_on_directories(true),
+    );
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:6050").await.unwrap();
     println!("Serving static files on http://localhost:6050");
-    
     axum::serve(listener, app).await.unwrap();
 }
