f6ae19445b2159b9952befb54202936dab785cfe
treymouledoux <72358826+treymouledoux@users.noreply.github.com>
2026-08-20 21:07:37 -0500

    Added compression layer to router

--- portfolio-serve/src/main.rs
+++ portfolio-serve/src/main.rs
@@ -1,11 +1,14 @@
 use axum::Router;
-use tower_http::services::ServeDir;
+use tower_http::{
+    compression::CompressionLayer,
+    services::ServeDir
+};
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().fallback_service(
-        ServeDir::new("assets").append_index_html_on_directories(true),
-    );
+    let app = Router::new()
+        .fallback_service(ServeDir::new("assets").append_index_html_on_directories(true))
+        .layer(CompressionLayer::new());
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:6050").await.unwrap();
     println!("Serving static files on http://localhost:6050");
